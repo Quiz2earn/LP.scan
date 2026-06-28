586788efa1763d0fa96a8a9fafe590a828ef3023
ER <116835982+Quiz2earn@users.noreply.github.com>
2026-06-28 23:55:27 +0400

    Create cotract_deploer.sol

---
+++ cotract_deploer.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract QUIZ2EARN {
+    uint256 public count;
+
+    event Incremented(address indexed user, uint256 newValue);
+
+    function increment() external {
+        count += 1;
+        emit Incremented(msg.sender, count);
+    }
+
+    function getCount() external view returns (uint256) {
+        return count;
+    }
+}
